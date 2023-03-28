a252a509c7dfe972a1aa432fbb25ef971b971bfc
Alex Doubov <alex.doubov@autodesk.com>
2023-03-28 08:15:39 -0700

    Create DriverInitializer property on VerifyMigrationTask

--- sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/VerifyMigrationTask.kt
+++ sqldelight-gradle-plugin/src/main/kotlin/app/cash/sqldelight/gradle/VerifyMigrationTask.kt
@@ -29,6 +29,8 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import java.io.File
+import java.io.Serializable
+import java.sql.DriverManager
 import java.util.ServiceLoader
 
 @CacheableTask
@@ -51,6 +53,8 @@ abstract class VerifyMigrationTask : SqlDelightWorkerTask() {
 
   @Input var verifyDefinitions: Boolean = true
 
+  @get:Internal abstract var driverInitializer: DriverInitializer
+
   /* Tasks without an output are never considered UP-TO-DATE by Gradle. Adding an output file that's created when the
    * task completes successfully works around the lack of an output for this task. There may be a better solution once
    * https://github.com/gradle/gradle/issues/14223 is resolved. */
@@ -68,6 +72,7 @@ abstract class VerifyMigrationTask : SqlDelightWorkerTask() {
         it.verifyMigrations.set(verifyMigrations)
         it.compilationUnit.set(compilationUnit)
         it.verifyDefinitions.set(verifyDefinitions)
+        it.driverInitializer.set(driverInitializer)
       }
       workQueue.await()
     }.onSuccess {
@@ -92,6 +97,7 @@ abstract class VerifyMigrationTask : SqlDelightWorkerTask() {
     val compilationUnit: Property<SqlDelightCompilationUnit>
     val verifyMigrations: Property<Boolean>
     val verifyDefinitions: Property<Boolean>
+    val driverInitializer: Property<DriverInitializer>
   }
 
   abstract class VerifyMigrationAction : WorkAction<VerifyMigrationWorkParameters> {
@@ -116,6 +122,7 @@ abstract class VerifyMigrationTask : SqlDelightWorkerTask() {
       if (!environment.dialect.isSqlite) return
       parameters.workingDirectory.get().asFile.deleteRecursively()
 
+      parameters.driverInitializer.get().execute()
       val catalog = createCurrentDb()
 
       val databaseFiles = sourceFolders.asSequence()
@@ -198,4 +205,12 @@ abstract class VerifyMigrationTask : SqlDelightWorkerTask() {
       }
     }
   }
+}
+
+/**
+ * Allows consumers to configure and register (with [DriverManager]) their custom drivers prior to
+ * running migration verification task.
+ */
+interface DriverInitializer : Serializable {
+  fun execute()
 }
